079ec07147209814bbc2c5c71c2950324b01cdfe
haemin <haemin7451@naver.com>
2025-03-30 20:08:50 +0900

    feat: 성공

--- src/main/java/e1/Main.java
+++ src/main/java/e1/Main.java
@@ -3,10 +3,9 @@ package e1;
 public class Main {
     public static void main(String[] args) {
         // 다형성을 활용한 메서드 호출 코드를 작성하세요.
-        Playable play = new MediaPlayer();
-        Pauseable pause = new MediaPlayer();
 
-        play.play();
-        pause.pause();
+        MediaPlayer mediaPlayer = new MediaPlayer();
+        mediaPlayer.play();
+        mediaPlayer.pause();
     }
 }
